75df71db073fccd0a5abc76baac9f3f7e0c893d9
angel <dewebisc@gmail.com>
2020-09-25 08:33:37 -0300

    Initial commit

---
+++ app/src/main/java/cl/dewebsic/marketplacedeventas/MainActivity.java
@@ -0,0 +1,94 @@
+package cl.dewebsic.marketplacedeventas;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    //declaramos los componentes
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle toggle;
+    Toolbar toolbar;
+    NavigationView navigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        drawerLayout=findViewById(R.id.drawer);
+        toolbar=findViewById(R.id.toolBar);
+        setSupportActionBar(toolbar);
+        toggle=new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.open,R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView=findViewById(R.id.nav_view);
+
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                int id=menuItem.getItemId();
+                Fragment fragment = null;
+
+                //evaluamos las opciones del menu
+                switch (id){
+                    case R.id.perfil:
+                        fragment = new PerfilFragment();
+                        loadFragment(fragment);
+                        break;
+                    case R.id.basket:
+                        fragment = new BasketFragment();
+                        loadFragment(fragment);
+                        break;
+                    case R.id.favorite:
+                        fragment = new FavoriteFragment();
+                        loadFragment(fragment);
+                        break;
+                    case R.id.code:
+                        fragment = new PromoCodeFragment();
+                        loadFragment(fragment);
+                        break;
+                    case R.id.orders:
+                        fragment = new OrderFragment();
+                        loadFragment(fragment);
+                        break;
+                    case R.id.setting:
+                        fragment = new SettingFragment();
+                        loadFragment(fragment);
+                        break;
+                    case R.id.support:
+                        fragment = new SupportFragment();
+                        loadFragment(fragment);
+                        break;
+                    default:
+                        break;
+                }
+                return true;
+            }
+        });
+    }
+
+    //método para cargar un fragment
+    private void loadFragment(Fragment fragment) {
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame,fragment).commit();
+        drawerLayout.closeDrawer(GravityCompat.START);
+        fragmentTransaction.addToBackStack(null);
+    }
+
+
+}
