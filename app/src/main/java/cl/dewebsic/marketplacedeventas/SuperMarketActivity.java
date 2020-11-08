04966782cce2ea0cc0c2ae9b2b5fa82ecbe03168
angel <dewebisc@gmail.com>
2020-11-07 23:38:37 -0300

    lista configuracion google maps api

--- app/src/main/java/cl/dewebsic/marketplacedeventas/SuperMarketActivity.java
+++ app/src/main/java/cl/dewebsic/marketplacedeventas/SuperMarketActivity.java
@@ -11,21 +11,25 @@ public class SuperMarketActivity extends AppCompatActivity {
 
 
     Button btnRegresar;
+    Button btnSearch;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_super_market_main);
 
+        btnSearch = findViewById(R.id.btnSearch);
 
         btnRegresar = findViewById(R.id.btnRegresar);
 
-        btnRegresar.setOnClickListener(new View.OnClickListener(){
+
+        btnSearch.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
-                Intent next = new Intent(SuperMarketActivity.this,MainActivity.class);
+                Intent next = new Intent(SuperMarketActivity.this, MapaTienda.class);
                 startActivity(next);
             }
         });
+
     }
 }
