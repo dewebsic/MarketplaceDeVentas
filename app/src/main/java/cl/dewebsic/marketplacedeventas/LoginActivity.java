f1c1024e622585a186fdbbc18a7ea2c290f6ebdd
angel <dewebisc@gmail.com>
2020-11-17 09:36:04 -0300

    se agregaron dependencias de conexion a firebase y creacion de activity para registro

--- app/src/main/java/cl/dewebsic/marketplacedeventas/LoginActivity.java
+++ app/src/main/java/cl/dewebsic/marketplacedeventas/LoginActivity.java
@@ -6,10 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
     Button btnRegresar;
+    TextView txtRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,11 +20,22 @@ public class LoginActivity extends AppCompatActivity {
 
         btnRegresar = findViewById(R.id.btnRegresar);
 
+        txtRegister = findViewById(R.id.register);
+
         btnRegresar.setOnClickListener(new View.OnClickListener(){
 
             @Override
             public void onClick(View view) {
                 Intent next = new Intent(LoginActivity.this,MainActivity.class);
+                startActivity(next);
+            }
+        });
+
+        txtRegister.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                Intent next = new Intent(LoginActivity.this,RegisterActivity.class);
                 startActivity(next);
             }
         });
