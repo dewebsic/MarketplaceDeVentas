0c2cdb948a182e50eeef25ecb6ba53834ea00f88
angel <dewebisc@gmail.com>
2020-11-22 17:11:46 -0300

    registro de usuario a firebase mejoras con ingreso nombre y creacion de colecion den database realtime

--- app/src/main/java/cl/dewebsic/marketplacedeventas/RegisterActivity.java
+++ app/src/main/java/cl/dewebsic/marketplacedeventas/RegisterActivity.java
@@ -16,13 +16,21 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RegisterActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
+    private DatabaseReference mDatabase;
 
     /* campos */
     private EditText password;
+    private EditText name;
     private EditText email;
     private EditText passwordConfirmation;
     private Button btnRegister;
@@ -33,10 +41,12 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
 
         mAuth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
 
         password = findViewById(R.id.passwordText);
         passwordConfirmation = findViewById(R.id.passwordRepitText);
         email = findViewById(R.id.emailText);
+        name = findViewById(R.id.nameText);
         btnRegister = findViewById(R.id.registerBtn);
 
         btnRegister.setOnClickListener(new View.OnClickListener(){
@@ -68,12 +78,37 @@ public class RegisterActivity extends AppCompatActivity {
                             if (task.isSuccessful()) {
                                 // Sign in success, update UI with the signed-in user's information
 
-                                Toast.makeText(getApplicationContext(),"Se ha registrado correctamente",Toast.LENGTH_SHORT).show();
-                                FirebaseUser user = mAuth.getCurrentUser();
+                                Map<String,Object> map = new HashMap<>();
+                                map.put("name", name.getText().toString());
+                                map.put("email", email.getText().toString());
+                                map.put("password", password.getText().toString());
+                                String id = mAuth.getCurrentUser().getUid();
 
-                                Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
+                                mDatabase.child("Users").child(id).setValue(map).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task2) {
 
-                                startActivity(intent);
+                                        if(task2.isSuccessful()){
+
+                                            Toast.makeText(getApplicationContext(),"Se ha registrado correctamente",Toast.LENGTH_SHORT).show();
+                                            FirebaseUser user = mAuth.getCurrentUser();
+
+                                            Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
+                                            startActivity(intent);
+
+                                        }else{
+
+                                                // If sign in fails, display a message to the user.
+
+                                                Toast.makeText(getApplicationContext(), "Authentication failed.",
+                                                        Toast.LENGTH_SHORT).show();
+                                                //updateUI(null);
+
+                                        }
+                                    }
+                                });
+
+
 
                                 //updateUI(user);
                             } else {
